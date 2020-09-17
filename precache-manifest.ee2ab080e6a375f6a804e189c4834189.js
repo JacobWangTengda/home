fa2ae3c395a29e0817950627a5a2c7f9026a8e3f
JacobWangTengda <wtd8207@gmail.com>
2020-09-17 22:44:45 +0800

    Updates

---
+++ precache-manifest.ee2ab080e6a375f6a804e189c4834189.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a432091a5c2e78ea873267b95749d73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "eadff985f7a744189136",
+    "url": "/static/css/main.5bc11802.chunk.css"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/static/js/2.8fa335fd.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/static/js/2.8fa335fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eadff985f7a744189136",
+    "url": "/static/js/main.93c0b40e.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "990dab0bafd27bf390e2596dd7b85b00",
+    "url": "/static/media/resume.990dab0b.pdf"
+  }
+]);
